e0196b91675c3da945cd20515132f4e7539336c2
Clive Chan <cc@clive.io>
2016-05-06 12:52:14 -0400

    fixes

--- src/main/java/com/cliveio/apcs/ServerStarter.java
+++ src/main/java/com/cliveio/apcs/ServerStarter.java
@@ -4,10 +4,10 @@ public class ServerStarter {
   public static void main(String[] args){
     try{
       HTTPServer hs = new HTTPServer(8000);
-      SocketServer ss = new SocketServer(1238);
+      SocketServer ss = new SocketServer(1234);
       ss.start();
       hs.start();
-      Thread.sleep(1000);
+      Thread.sleep(Integer.MAX_VALUE);
       ss.interrupt();
       hs.interrupt();
     }catch(Exception e){
