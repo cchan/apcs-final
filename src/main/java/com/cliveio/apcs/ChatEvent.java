2e9aa19be2bae693834ef5d79d9856dc4c097738
Clive Chan <cc@clive.io>
2016-05-23 00:37:44 -0400

    xss-proof

--- src/main/java/com/cliveio/apcs/ChatEvent.java
+++ src/main/java/com/cliveio/apcs/ChatEvent.java
@@ -4,12 +4,12 @@ import java.util.Date;
 public class ChatEvent extends Event{
   private String author;
   public String getAuthor(){return author;}
-  public void setAuthor(String author){this.author=author;}
+  public void setAuthor(String author){this.author = sanitize(author);}
   
   private String message;
   public String getMessage(){return message;}
-  public void setMessage(String message){this.message=message;}
+  public void setMessage(String message){this.message = sanitize(message);}
   
   public ChatEvent(){}
-  public ChatEvent(String author, String message){super(); this.author = author; this.message = message;}
+  public ChatEvent(String author, String message){super(); this.author = sanitize(author); this.message = sanitize(message);}
 }
